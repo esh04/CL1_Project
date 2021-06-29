b186e2457badfbbc86661e68c492aeef1de563f5
Esh <eshika.k@research.iiit.ac.in>
2021-06-29 22:11:34 +0530

    updated main

--- English/main.py
+++ English/main.py
@@ -1,4 +1,3 @@
-from Hindi.main import POS_tags
 import string
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem.porter import PorterStemmer
@@ -10,6 +9,127 @@ from collections import Counter
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
+#remove links (if any)
+def remove_links(text):
+    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+
+#tokenisation
+def tokenize_sentence(text):
+    sentences = sent_tokenize(text)
+    return sentences
+
+def tokenize_word(text):
+    tokens = word_tokenize(text)
+    
+    #uncommment to store tokens in a file
+
+    with open('./data/tokens.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % token for token in tokens)
+
+    return tokens
+
+#cleaning the tokens
+def clean(tokens):
+    #convert to lower case
+    tokens = [w.lower() for w in tokens]
+
+    # remove punctuation from each word
+
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    # remove remaining tokens that are not alphabetic 
+    words = [word for word in stripped if word.isalpha()]
+
+    English_words = set(nltk.corpus.words.words())
+    #adding words relevent to current topic
+
+    English_words.add('coronavirus')
+    English_words.add('covishield')
+    English_words.add('wuhan')
+    English_words.add('covaxin')
+    English_words.add('pfizer')
+    English_words.add('sputnik')
+    English_words.add('pfizerbiontech')
+    English_words.add('johnson')
+
+
+    #remove foreign words
+    words = [word for word in stripped if word in English_words ]
+
+    # FILTERING OUT STOPWORDS
+    stop_words = set(stopwords.words('english'))
+    stop_words.add('al')
+    stop_words.add('may')
+    stop_words.add('might')
+    stop_words.add('also')
+    stop_words.add('could')
+    stop_words.add('like')
+    stop_words.add('said')
+
+    words = [w for w in words if not w in stop_words]
+
+    #uncommment to store cleaned tokens in a file
+
+    with open('./data/clean_tokens.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % word for word in words)
+
+    return words
+
+def stemmer(words):
+    #stemming
+    stemmer = PorterStemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in words]
+
+    #uncommment to store stemmed tokens in a file
+
+    with open('./data/stem.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % stem for stem in stemmed_tokens)
+
+    return stemmed_tokens
+
+def lemmatizer(words):
+    #lemmatization
+    lm = WordNetLemmatizer()
+    lemmatized_tokens = [lm.lemmatize(token) for token in words]
+
+    #uncommment to store stemmed tokens in a file
+
+    with open('./data/lemm.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % lemm for lemm in lemmatized_tokens)
+
+    return lemmatized_tokens
+
+def POS_tagging(words):
+    POS_tagged = nltk.pos_tag(words)
+    return POS_tagged
+
+
+def wordcloud(words,POS_tagged):
+    
+
+    #Making the wordcloud
+    cloud_words = []
+    tags = ['NN','JJ']
+
+    #only add those words to wordcloud that are nouns/adjectives
+    for token in POS_tagged:
+        if token[1] in tags:
+            cloud_words.append(token[0])
+
+    dictionary=Counter(cloud_words)
+    cloud = WordCloud(max_font_size=80,colormap="hsv",width=600, height=400,background_color="white").generate_from_frequencies(dictionary)
+    plt.figure(figsize=(20,15))
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+def Frequency_graphs(tokens):
+   
+    fd = nltk.FreqDist(tokens)
+    #graph will have 50 entries
+    fd.plot(50,cumulative=False)
+
+##########################################################
 
 #English_data.txt is the scraped English data.
 filename = 'English_data.txt'
@@ -17,128 +137,54 @@ file = open(filename, 'rt')
 text = file.read()
 file.close()
 
-####################################################
+remove_links(text)
+sent = tokenize_sentence(text)
+tokens = tokenize_word(text)
 
-#remove links
-text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+#uncomment to view frequency graph of initial tokens
+Frequency_graphs(tokens)
 
-####################################################
+cleaned_tokens = clean(tokens)
 
-# TOKENISATION
-sentences = sent_tokenize(text)
-tokens = word_tokenize(text)
+#uncomment to view frequency graph of cleaned tokens
+Frequency_graphs(cleaned_tokens)
 
+stemm_tokens = stemmer(cleaned_tokens)
 
-#########################################
+#uncomment to view frequency graph of stemmed tokens
+Frequency_graphs(stemm_tokens)
 
-#Cleaning the corpus
+lemm_tokens = lemmatizer(cleaned_tokens)
 
-# convert to lower case
-tokens = [w.lower() for w in tokens]
-# remove punctuation from each word
-table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in tokens]
-# remove remaining tokens that are not alphabetic 
-words = [word for word in stripped if word.isalpha()]
+#uncomment to viewfrequency graph of lemmatized tokens
+Frequency_graphs(lemm_tokens)
 
-English_words = set(nltk.corpus.words.words())
-#adding words relevent to current topic
+POS_tagged_tokens = POS_tagging(cleaned_tokens)
 
-English_words.add('coronavirus')
-English_words.add('covishield')
-English_words.add('wuhan')
-English_words.add('covaxin')
-English_words.add('pfizer')
-English_words.add('sputnik')
-English_words.add('pfizerbiontech')
-English_words.add('johnson')
+#uncomment to form a frequency graph of all the POS_tags used
+POS_tags = [list[1] for list in POS_tagged_tokens]
+Frequency_graphs(POS_tags) 
 
 
-#remove foreign words
-words = [word for word in stripped if word in English_words ]
+wordcloud(cleaned_tokens,POS_tagged_tokens)
 
 
-#################################################
+##################################################################
+
+#uncomment to form a frequency graph of a pre-processed token file present in data file (file name 'token' in this case) 
 
 
-# FILTERING OUT STOPWORDS
-stop_words = set(stopwords.words('english'))
-stop_words.add('al')
-stop_words.add('may')
-stop_words.add('might')
-stop_words.add('also')
-stop_words.add('could')
-stop_words.add('like')
-stop_words.add('said')
+# with open("./data/tokens.txt") as f:
+#     tokens = [ line.strip() for line in f ]
 
-words = [w for w in words if not w in stop_words]
+# Frequency_graphs(tokens)
 
+#uncomment to form a wordcloud of a pre-processed token file present in data folder (file name 'token' in this case)
 
-################################################
+# with open("./data/tokens.txt") as f:
+#     tokens = [ line.strip() for line in f ]
 
-#Stemming and lemmatization
-
-#stemming
-stemmer = PorterStemmer()
-stemmed_tokens = [stemmer.stem(token) for token in words]
-
-#lemmatization
-lm = WordNetLemmatizer()
-lemmatized_tokens = [lm.lemmatize(token) for token in words]
+# wordcloud(tokens,POS_tagging(tokens))
 
 
-####################################################
 
-#POS tagging
-POS_tagged = nltk.pos_tag(words)
-
-#Making the wordcloud
-cloud_words = []
-tags = ['NN','JJ']
-
-#only add those words to wordcloud that are nouns/adjectives
-for token in POS_tagged:
-    if token[1] in tags:
-        cloud_words.append(token[0])
-
-dictionary=Counter(cloud_words)
-cloud = WordCloud(max_font_size=80,colormap="hsv",width=600, height=400,background_color="white").generate_from_frequencies(dictionary)
-plt.figure(figsize=(20,15))
-plt.imshow(cloud, interpolation='bilinear')
-plt.axis('off')
-plt.show()
-
-
-#to make graphs (uncomment whichever graph you wish to make)
-#different lists denote the state of data at that point
-
-# fd = nltk.FreqDist(tokens)
-
-# fd = nltk.FreqDist(words)
-
-# fd = nltk.FreqDist(stemmed_tokens)
-
-# fd = nltk.FreqDist(lemmatized_tokens)
-
-# POS_tags = [list[1] for list in POS_tagged]
-# fd = nltk.FreqDist(POS_tags)
-
-#and uncomment this line:
-# fd.plot(100,cumulative=False)
-
-#to store any of these in text files
-
-with open('./data/tokens.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % token for token in tokens)
-
-with open('./data/clean_tokens.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % word for word in words)
-
-with open('./data/stem.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % stem for stem in stemmed_tokens)
-
-with open('./data/lemm.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % lemm for lemm in lemmatized_tokens)
-
-with open('./data/POS.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % pos for pos in POS_tags)
--- Hindi/main.py
+++ Hindi/main.py
@@ -20,143 +20,204 @@ font="./font/Lohit-Devanagari.ttf"
 
 ###################################################3
 
+#remove links (if any)
+def remove_links(text):
+    text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
+
+#tokenisation
+def tokenize_sentence(text):
+    sentences = sent_tokenize(text)
+    return sentences
+
+def tokenize_word(text):
+    tokens = word_tokenize(text)
+    
+    #uncommment to store tokens in a file
+
+    with open('./data/tokens.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % token for token in tokens)
+
+    return tokens
+
+#cleaning the tokens
+def clean(tokens):
+    # remove punctuation from each word
+    #adding some additional punctuations
+    string.punctuation += '।'
+    string.punctuation += '“'
+    string.punctuation += '”'
+
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+
+    # remove remaining tokens that are in English or are numbers 
+    words = [word for word in stripped if not word.isalnum()]
+
+
+    # FILTERING OUT STOPWORDS
+
+    filename = './data/stopwords.txt'
+    file = open(filename, 'rt')
+    stop_words = file.read()
+    file.close()
+
+
+    words = [w for w in words if not w in stop_words]
+
+    #spacy-stopwords removal
+    words = [word for word in words if word not in set(STOP_WORDS_HI) ]
+
+    #uncommment to store cleaned tokens in a file
+
+    with open('./data/clean_tokens.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % word for word in words)
+
+    return words
+
+
+def stemmer(words):
+    #stemming
+    stemmed_tokens = []
+    string = ' '.join([str(elem) for elem in words])
+    doc = nlp_Spacy(string)
+    for token in doc:
+        stemmed_tokens.append(token.norm_)
+    
+    #uncommment to store stemmed tokens in a file
+
+    with open('./data/stem.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % stem for stem in stemmed_tokens)
+
+    return stemmed_tokens
+
+            
+#lemmatization
+def lemmatizer(words):
+    lemm_tokens = []
+    string = ' '.join([str(elem) for elem in words])
+    doc = nlp(string)
+
+    for sent in doc.sentences:
+        for word in sent.words:
+            lemm_tokens.append(word.lemma)
+
+    #uncommment to store stemmed tokens in a file
+
+    with open('./data/lemm.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % lemm for lemm in lemm_tokens)
+
+    return lemm_tokens
+
+
+
+#POS tagging
+def POS_tagging(words):
+
+    POS_tags = []
+
+    string = ' '.join([str(elem) for elem in words])
+    doc = nlp(string)
+    for sent in doc.sentences:
+        for word in sent.words:
+            POS_tags.append(word.xpos)
+    
+    
+    return POS_tags
+
+def wordcloud(words):
+    #only nouns and adjectives will be included in the wordcloud
+    cloud_words = []
+    tags = ['NN','JJ','NNC','NNP','NNPC']
+    string = ' '.join([str(elem) for elem in words])
+    doc = nlp(string)
+
+    for sent in doc.sentences:
+        for word in sent.words:
+            if word.xpos in tags:
+                cloud_words.append(word.text)
+
+
+    #word cloud
+
+    dictionary=Counter(cloud_words)
+    cloud = WordCloud(max_font_size=80,colormap="hsv",width=600, height=400,background_color="white",font_path=font).generate_from_frequencies(dictionary)
+    plt.figure(figsize=(20,15))
+    plt.imshow(cloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.show()
+
+
+def Frequency_graphs(tokens):
+   
+    fd = nltk.FreqDist(tokens)
+    # to print hindi words:
+    plt.rcParams['font.sans-serif']=['gargi'] 
+    plt.rcParams['axes.unicode_minus']=False
+
+    #graph will have 50 entries
+    fd.plot(50,cumulative=False)
+
+################################################################
+
 #the scraped data will be stored in this file: Hindi_data.txt
 filename = 'Hindi_data.txt'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
 
-###################################################
-
-# CLEANING AND TOKENISATION
-
-#remove links (if any)
-text = re.sub(r'^https?:\/\/.*[\r\n]*', '', text, flags=re.MULTILINE)
-
-#tokenisation
-sentences = sent_tokenize(text)
-tokens = word_tokenize(text)
 
 
-# remove punctuation from each word
-#adding some additional punctuations
-string.punctuation += '।'
-string.punctuation += '“'
-string.punctuation += '”'
+remove_links(text)
+sent = tokenize_sentence(text)
+tokens = tokenize_word(text)
 
-table = str.maketrans('', '', string.punctuation)
-stripped = [w.translate(table) for w in tokens]
+#uncomment to view frequency graph of initial tokens
+Frequency_graphs(tokens)
 
-# remove remaining tokens that are in English or are numbers 
-words = [word for word in stripped if not word.isalnum()]
+cleaned_tokens = clean(tokens)
 
+#uncomment to view frequency graph of cleaned tokens
+Frequency_graphs(cleaned_tokens)
 
-#################################################
+stemm_tokens = stemmer(cleaned_tokens)
 
+#uncomment to view frequency graph of stemmed tokens
+Frequency_graphs(stemm_tokens)
 
-# FILTERING OUT STOPWORDS
+lemm_tokens = lemmatizer(cleaned_tokens)
 
-filename = './data/stopwords.txt'
-file = open(filename, 'rt')
-stop_words = file.read()
-file.close()
+#uncomment to viewfrequency graph of lemmatized tokens
+Frequency_graphs(lemm_tokens)
 
+POS_tags = POS_tagging(cleaned_tokens)
 
-words = [w for w in words if not w in stop_words]
-
-#spacy-stopwords removal
-words = [word for word in words if word not in set(STOP_WORDS_HI) ]
+#uncomment to form a frequency graph of all the POS_tags used
+Frequency_graphs(POS_tags) 
 
 
-################################################
+wordcloud(cleaned_tokens)
 
 
-#stemming
-stemmed_tokens = []
-string = ' '.join([str(elem) for elem in words])
-doc = nlp_Spacy(string)
-for token in doc:
-    stemmed_tokens.append(token.norm_)
+##################################################################
 
-###################################################
+#uncomment to form a frequency graph of a pre-processed token file present in data file (file name 'token' in this case) 
 
-#TO LOAD ANY PRE-EXISTING TOKEN FILE (if you have preprocessed data you can comment out the code above this comment, and just load your file)
-#loading data
-# strip() removes the trailing \n
 
-# with open("./data/clean_tokens.txt") as f:
-#     clean_tokens = [ line.strip() for line in f ]
+# with open("./data/tokens.txt") as f:
+#     tokens = [ line.strip() for line in f ]
+
+# Frequency_graphs(tokens)
+
+#uncomment to form a wordcloud of a pre-processed token file present in data folder (file name 'token' in this case)
+
+# with open("./data/tokens.txt") as f:
+#     tokens = [ line.strip() for line in f ]
+
+# wordcloud(tokens)
 
 
 
-#POS tagging
-POS_tags = []
-lemm_tokens = []
-string = ' '.join([str(elem) for elem in words])
-doc = nlp(string)
-
-###############################################
-
-#only nouns and adjectives will be included in the wordcloud
-cloud_words = []
-tags = ['NN','JJ','NNC','NNP','NNPC']
 
 
-for sent in doc.sentences:
-    for word in sent.words:
-        POS_tags.append(word.xpos)
-        #LEMMETIZATION
-        lemm_tokens.append(word.lemma)
-        if word.xpos in tags:
-            cloud_words.append(word.text)
-
-
-#word cloud
-
-dictionary=Counter(cloud_words)
-cloud = WordCloud(max_font_size=80,colormap="hsv",width=600, height=400,background_color="white",font_path=font).generate_from_frequencies(dictionary)
-plt.figure(figsize=(20,15))
-plt.imshow(cloud, interpolation='bilinear')
-plt.axis('off')
-plt.show()
-
-
-#to make graphs (uncomment whichever graph you wish to make)
-#different lists denote the state of data at that point
-
-fd = nltk.FreqDist(lemm_tokens)
-
-fd = nltk.FreqDist(stemmed_tokens)
-
-fd = nltk.FreqDist(tokens)
-
-fd = nltk.FreqDist(words)
-
-fd = nltk.FreqDist(POS_tags)
-
-# to print hindi words:
-
-# also, uncomment these lines
-plt.rcParams['font.sans-serif']=['gargi'] 
-plt.rcParams['axes.unicode_minus']=False
-fd.plot(50,cumulative=False)
-
-
-#to store any of these in text files
-
-with open('./data/tokens.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % token for token in tokens)
-
-with open('./data/clean_tokens.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % word for word in words)
-
-with open('./data/stem.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % stem for stem in stemmed_tokens)
-
-with open('./data/lemm.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % lemm for lemm in lemm_tokens)
-
-with open('./data/POS.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % pos for pos in POS_tags)
 
 
